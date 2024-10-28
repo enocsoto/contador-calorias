94b623a68ff92f9bd4f54531098a5ff3133770b6
Enoc de Jesus Soto Arenilla <tec-enocdejesus@hotmail.com>
2024-10-28 11:16:37 -0300

    add: calculo de calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import type { Activity } from "../types"
+import CaloriesDisplay from "./CaloriesDisplay"
 
 type CalorieTrackerProps = {
   activities: Activity[],
@@ -7,19 +8,27 @@ type CalorieTrackerProps = {
 
 const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
   const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
+  const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
+  const calculateTotal = useMemo(() => caloriesBurned - caloriesConsumed, [activities]);
+
   return (
     <>
       <h2 className="text-4xl from-neutral-900 text-white text-center">
         Resumen de Calorias</h2>
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-white">
-            {caloriesConsumed}
-          </span>
-          Consumidas
-        </p>
+        <CaloriesDisplay
+          calories={caloriesConsumed}
+          text='Consumidas'
+        />
 
-
+        <CaloriesDisplay
+          calories={caloriesBurned}
+          text='Quemadas'
+        />
+        <CaloriesDisplay
+          calories={calculateTotal}
+          text='Total calorias quemadas'
+        />
       </div>
     </>
   )
